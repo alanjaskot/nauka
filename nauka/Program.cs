1ec538b136658f78ab6b3ce94da6f9bd39069617
alanjaskot <alanjaskot@gmail.com>
2021-05-12 21:30:20 +0200

    Dodanie pustego projektu

---
+++ nauka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nauka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
